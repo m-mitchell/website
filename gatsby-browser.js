3d66a67dd2e5443755401dd949090e0f94126b5e
Griko Nibras <grikomsn@gmail.com>
2019-08-19 19:46:17 +0700

    Remove context and wrapper

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,17 +1,7 @@
-import React from 'react'
-
-import AppProvider from './src/store/provider'
-
 export const onClientEntry = () => {
   // IntersectionObserver polyfill for gatsby-background-image (Safari, IE)
   if (typeof window.IntersectionObserver === 'undefined') {
     import('intersection-observer')
     console.log('# IntersectionObserver is polyfilled!')
   }
-}
-
-// https://www.gatsbyjs.org/docs/browser-apis/#wrapRootElement
-// eslint-disable-next-line react/prop-types
-export const wrapRootElement = ({ element }) => {
-  return <AppProvider>{element}</AppProvider>
 }
