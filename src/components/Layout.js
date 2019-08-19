4ee785be0b2bc046a376259e78d6a72e302426b2
Griko Nibras <grikomsn@gmail.com>
2019-08-19 20:12:35 +0700

    Remove font package and use google fonts

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,26 +1,19 @@
+import '../stylesheets/style.css'
 import 'tachyons/css/tachyons.min.css'
-import 'typeface-ibm-plex-sans'
 
 import React from 'react'
-import styled from 'styled-components'
 
 import Footer from './Footer'
 import Head from './Head'
 
-const Wrapper = styled.div`
-  font-family: 'IBM Plex Sans', -apple-system, BlinkMacSystemFont, 'avenir next',
-    avenir, 'helvetica neue', helvetica, ubuntu, roboto, noto, 'segoe ui', arial,
-    sans-serif !important;
-`
-
 const Layout = ({ beforeChildren, ...props }) => (
   <>
     <Head />
-    <Wrapper className="center dark-gray flex flex-column justify-between min-vh-100 sans-serif">
+    <div className="center dark-gray flex flex-column justify-between min-vh-100 sans-serif">
       {beforeChildren}
       <main {...props} />
       <Footer />
-    </Wrapper>
+    </div>
   </>
 )
 
